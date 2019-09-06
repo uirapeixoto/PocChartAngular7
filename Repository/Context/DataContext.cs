d931470b8e3b65fd77ccbc5926070ee3e627e8c9
uirapeixoto <uira.peixoto@gmail.com>
2019-09-06 17:19:36 -0300

    fix security

--- Repository/Context/DataContext.cs
+++ Repository/Context/DataContext.cs
@@ -25,8 +25,7 @@ namespace Repository.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                //optionsBuilder.UseSqlServer("Server=wizposvenda001-hml-sqlserver.database.windows.net;Initial Catalog=SQG;Persist Security Info=False;User ID=usrAppSQG;Password=kcQBwev5TAjMn7wP;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                
             }
         }
 
